aa1bedfd7f673e4345f957aa0d2729c4b82dad90
“tatianamih” <tmihalchenko66@gmail.com>
2020-09-19 19:42:14 +0300

    test with task about copy array

---
+++ level4_copy/src/test/java/jrr/by/service/CopyTest.java
@@ -0,0 +1,27 @@
+package jrr.by.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CopyTest {
+
+    @Test
+    public void copyInRange() {
+        Copy copy = new Copy();
+        int[] array = {40, 50, 100, 200};
+        int[] expected = {50, 100};
+        int[] actual = copy.copyInRange(array, 41, 150);
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void copyInRangeRunLenghtOfNewArray() {
+        Copy copy = new Copy();
+        int[] array = {40, 50, 100, 200};
+        int expected = 2;
+        int actual = copy.copyInRangeRunLenghtOfNewArray(array, 41, 150);
+        assertEquals(expected, actual);
+    }
+}
