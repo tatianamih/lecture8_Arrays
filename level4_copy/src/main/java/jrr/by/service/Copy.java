f8418c97c61a7294816f2f65d3b91b2eedd3586e
“tatianamih” <tmihalchenko66@gmail.com>
2020-09-19 19:41:25 +0300

    my task about copy array

---
+++ level4_copy/src/main/java/jrr/by/service/Copy.java
@@ -0,0 +1,37 @@
+package jrr.by.service;
+
+public class Copy {
+    public int copyInRangeRunLenghtOfNewArray(int[] array, int leftBound, int rightBound){
+        int newArraySize = 0;
+
+        for (int j = 0; j < array.length; j++){
+            for (int i = leftBound; i<= rightBound; i++){
+
+                if (array[j] == i){
+                    newArraySize ++;
+                }
+            }
+        }
+        return newArraySize;
+    }
+
+    public int[] copyInRange(int[] array, int leftBound, int rightBound){
+
+        int newArraySize = copyInRangeRunLenghtOfNewArray(array, leftBound, rightBound);
+        int[] newArray = new int[newArraySize];
+        int indNewArray = 0;
+
+        for (int j = 0; j < array.length; j++){
+            for (int i = leftBound; i<= rightBound; i++){
+
+                if (array[j] == i){
+                    newArray[indNewArray] = i;
+                    indNewArray++;
+                }
+            }
+        }
+        return newArray;
+    }
+}
+
+
